66b9981151e6c2f7882fda84fa0abe7493da675d
Sardorbek Olimov <sardorbek.programming@gmail.com>
2026-03-17 23:51:45 +0500

    feat(navbar): add dynamic count badge for wishlist and cart

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,20 +4,30 @@ import { Button } from "@/components/ui/button";
 import Categories from "../categories/Categories";
 import Search from "../search/Search";
 import { Logo } from "@/data/Logo";
+import { IconName } from "../ui/icons";
+
+interface NavLink {
+  href: string;
+  icon: IconName;
+  label: string;
+  mr: boolean;
+  count?: number;
+}
 
 export default function Navbar() {
   const tPages = useTranslations("pages");
 
-  const navLinks = [
+  const navLinks: NavLink[] = [
     {
       href: "/wishlist",
       icon: "wishlist",
       label: tPages("wishlist"),
       mr: true,
+      count: 0,
     },
-    { href: "/cart", icon: "cart", label: tPages("cart"), mr: true },
+    { href: "/cart", icon: "cart", label: tPages("cart"), mr: true, count: 0 },
     { href: "/login", icon: "login", label: tPages("login"), mr: false },
-  ] as const;
+  ];
 
   return (
     <nav className="my-container row-between">
@@ -30,15 +40,21 @@ export default function Navbar() {
       <Search />
 
       <div className="flex items-center">
-        {navLinks.map(({ href, icon, label, mr }) => (
+        {navLinks.map(({ href, icon, label, mr, count }) => (
           <Button
             as={Link}
             key={href}
             href={href}
             leftIcon={icon}
-            className={`bg-card hover:bg-card-hover h-10 px-4 ${mr ? "mr-4" : ""}`}
+            className={`relative bg-card hover:bg-card-hover h-10 px-4 ${mr ? "mr-4" : ""}`}
           >
             {label}
+
+            {typeof count === "number" && (
+              <span className="absolute -top-2 -right-2 h-5 min-w-5 px-1 row-center rounded-full bg-primary text-[10px] text-white">
+                {count}
+              </span>
+            )}
           </Button>
         ))}
       </div>
